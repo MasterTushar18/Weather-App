3cffd9e598eaa24df62eb0bf71a8d9a7818a6635
Tushar <tushar25999@gmail.com>
2025-07-13 15:40:06 +0530

    Code refactoring

--- src/main/java/com/tushar/weather/controller/WeatherController.java
+++ src/main/java/com/tushar/weather/controller/WeatherController.java
@@ -2,13 +2,11 @@ package com.tushar.weather.controller;
 
 import com.tushar.weather.model.WeatherResponse;
 import com.tushar.weather.service.WeatherService;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class WeatherController {
 
     private final WeatherService weatherService;
@@ -19,12 +17,11 @@ public class WeatherController {
     }
 
     @GetMapping("/api/weather")
-    @ResponseBody
     public WeatherResponse getWeather(@RequestParam String city) {
 
         log.info("Inside class Controller in getWeather().");
         WeatherResponse weatherResponse = weatherService.getWeather(city);
-        log.info("Weather data fetched successfully for city: {}", weatherResponse.toString());
+        log.info("Weather data fetched successfully for city: {}", weatherResponse.getLocation().getName());
 
         return weatherResponse;
     }
